226308ac7f55db405f1ce534b580097504c540b8
nordstromkristofer <kristofernordstrom@Kristofers-MBP.lan>
2022-01-21 16:51:34 +0100

    ny json

--- jensen-backend/app.js
+++ jensen-backend/app.js
@@ -2,13 +2,13 @@
 const express = require("express")
 const app = express()
 
-process.env.PORT = 3000
+process.env.PORT = 5500
 
 app.get("/", (req,res)=>{
     res.status(200).send({"Status":"Framgång!"})
 })
 
-app.listen(3000, ()=>{
+app.listen(5500, ()=>{
     console.log(`Lyssnar på port ${process.env.PORT}`)
 });
 
