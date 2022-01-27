6318f2a834164d8b8976fa4c41ec1a891a36fbc2
nordstromkristofer <nordstromkristofer@gmail.com>
2022-01-27 16:27:57 +0100

    user api mongoose schema

--- jensen-backend/app.js
+++ jensen-backend/app.js
@@ -7,73 +7,70 @@ const jwt = require('jsonwebtoken')
 const https = require ('https')
 const path = require('path')
 const fs = require('fs')
+const mongoose = require ('mongoose')
+const dotenv = require ('dotenv');
+
+dotenv.config();
+
+//db connect
+mongoose.connect(
+   process.env.DB_CONNECT,
+   () => console.log('DB connectad iaf!')
+);
+
+//audit
+// var mongoose = require('mongoose');
+// var auditLog = require('audit-log');
+// auditLog.addTransport("mongoose");
+// mongoose.connect(
+//    'mongodb+srv://koko:kluster1@kluster1.xx1gc.mongodb.net/Wthr5?retryWrites=true&w=majority' ,{ useNewUrlParser: true}, () => 
+//    console.log('mongoDB funkar!'));
+//    schema = new mongoose.Schema({ ... });
+// // mongosh "mongodb+srv://kluster1.xx1gc.mongodb.net/myFirstDatabase" --username <username>
+// // either or both -- up to you where your messages are sent!
+// auditLog.addTransport("console");
 
 
 
-const app = express()
-// const PORT = process.env.PORT || 5500
+ const app = express()
+ const PORT = process.env.PORT || 5500
+ app.listen(PORT , ()=>{
+      console.log(`Lyssnar på port ${PORT}`)
+ });
 
-app.use(function (req, res, next) {
+
+
+ //import routes
+const authRoute = require ('./routes/auth');
+
+
+//route middlewares
+app.use('/api/user', authRoute);
+
+
+
+
+//heroku
+ app.use(function (req, res, next) {
    res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'");
-   next();
-});
+    next();
+ });
 
-app.use('/healthcheck', require('./routes/healthcheck.routes'));
+ app.use('/healthcheck', require('./routes/healthcheck.routes'));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+ app.use(express.urlencoded({ extended: true }));
+ app.use(cors())
 
 
-const sslServer = https.createServer({
-   key: fs.readFileSync(path.join(__dirname, 'certificates', 'key.pem')),
-   cert: fs.readFileSync(path.join(__dirname, 'certificates', 'cert.pem')),
-   },
-   app
-)
 
-sslServer.listen(5500,()=> console.log('Säker server på 5500'))
 // SSL
-
-
-app.use('/', (req,res,next)=>{
-   res.send('Tjena från SSL')
-})
-
-
-app.get("/", (req ,res)=>{
-   headers={"http_status":200, "cache-control":  "no-cache"}
-   body={"status": "available"}
-   res.status(200).send(body)
-})
-
-app.get("/health", (req ,res)=>{
-   headers={"http_status":200, "cache-control":  "no-cache"}
-   body={"status": "framgång"}
-   res.status(200).send(body)
-})
-
-
-app.post('/authorize', (req, res) => {
-   // Insert Login Code Here
-   let user = req.body.user;
-   let password = req.body.password;
-   console.log(`User ${user}`)
-   console.log(`Password ${password}`)
-
-   if(user===credentials.secretUser && password===credentials.secretPassword){
-      console.log("Authorized")
-      const token = jwt.sign({
-            data: 'foobar'
-      }, 'your-secret-key-here', { expiresIn: 60 * 60 }); 
-
-      console.log(token)
-      res.status(200).send(token)
-  }else{
-      console.log("Not authorized")
-      res.status(200).send({"STATUS":"FAILURE"})
-   }
-});
-
-// app.listen(PORT , ()=>{
-//      console.log(`Lyssnar på port ${PORT}`)
-// });
+ const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'certificates', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'certificates', 'cert.pem')),
+    },
+    app
+ )
+ sslServer.listen(5000,()=> console.log('Säker server på 5000'))
+ app.use('/', (req,res,next)=>{
+    res.send('Tjena från SSL')
+ });
---
+++ jensen-backend/routes/auth.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/register', (req, res) => {
+    res.send('Register');
+});
+
+router.post
+
+
+
+module.exports = router;
