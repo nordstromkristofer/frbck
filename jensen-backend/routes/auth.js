dabbdf0fe51988cf9cfa87ac12435cfddccca7f1
nordstromkristofer <nordstromkristofer@gmail.com>
2022-01-27 18:37:24 +0100

    lösenordhash login början

--- jensen-backend/routes/auth.js
+++ jensen-backend/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation} = require('../validation');
 
 
@@ -11,19 +12,33 @@ router.post('/register', async (req, res) => {
     const { error } = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
+    //kollar om redan reggad
 
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send ('Email finns redan');
+
+    //Hash lösenord
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Ny användare
      const user = new User({
          name: req.body.name,
         email: req.body.email,
-         password: req.body.password
+         password: hashedPassword
      });
      try {
         const savedUser = await user.save();
-         res.send(savedUser);
+         res.send({ user: user._id});
      }   catch (err){
          res.status(400).send(err);
      }
 });
+
+//LOGIN
+
+
 
 
 
