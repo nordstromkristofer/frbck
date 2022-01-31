83c4507be3458321b0f205dc2f5766d1a7055b84
nordstromkristofer <nordstromkristofer@gmail.com>
2022-01-31 17:31:44 +0100

    reg med hashat lösen

--- jensen-backend/routes/users.js
+++ jensen-backend/routes/users.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const bp = require('body-parser')
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 router.use(express.json());
 router.use(bp.json());
 router.use(bp.urlencoded({ extended: true }))
+
+const User = require('../model/User');
 
 
 //Login
@@ -35,9 +38,45 @@ router.post('/register', (req, res) => {
 
         });
     }else {
-        res.send('inloggad!!')
-    }
-});
+        User.findOne({ email: email }).then(user => {
+          if (user) {
+            errors.push({ msg: 'Email already exists' });
+            res.render('register', {
+              errors,
+              name,
+              email,
+              password
+            });
+          } else {
+            const newUser = new User({
+              name,
+              email,
+              password
+            });
+
+            //hash pass
+
+            bcrypt.genSalt(10, (err, salt) => 
+            bcrypt.hash(newUser.password, salt, (err, hash)  => {
+                if (err) throw err;
+
+                newUser.password = hash;
+
+                newUser.save()
+                .then(user => {res.redirect('/users/login');
+                })
+                .catch(err => console.log(err))
+                
+            
+            
+
+            }))
+                
+              
+            }
+          });
+        }
+      });
 
 module.exports = router;
 
